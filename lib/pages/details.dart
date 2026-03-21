0e1294dedeea2a7f463c02db329eaaec528ca037
vegardan <mail@vegardandersen.com>
2026-03-21 15:51:54 +0100

    Add edit note page

--- lib/pages/details.dart
+++ lib/pages/details.dart
@@ -1,17 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:ikt205g26v_02/pages/edit_note.dart';
 import 'package:ikt205g26v_02/storage/note.dart';
 
-class DetailsPage extends StatelessWidget {
+class DetailsPage extends StatefulWidget {
   final Note note;
 
   const DetailsPage(this.note, {super.key});
 
   @override
+  State<DetailsPage> createState() => _DetailsPageState();
+}
+
+class _DetailsPageState extends State<DetailsPage> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(elevation: 6, title: Text(note.title)),
+      appBar: AppBar(
+        elevation: 6,
+        title: Text(widget.note.title),
+        actions: [
+          IconButton(
+            onPressed: () async {
+              bool updated = await Navigator.of(context).push(MaterialPageRoute(builder: (context) => EditNotePage(widget.note)));
+
+              if (updated) {
+                setState(() {});
+              }
+            },
+            icon: Icon(Icons.edit_note),
+          ),
+        ],
+      ),
       body: SingleChildScrollView(
-        child: Padding(padding: const EdgeInsets.all(16), child: Text(note.description)),
+        child: Padding(padding: const EdgeInsets.all(16), child: Text(widget.note.text)),
       ),
     );
   }
---
+++ lib/pages/edit_note.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:ikt205g26v_02/storage/note.dart';
+import 'package:ikt205g26v_02/storage/note_service.dart';
+
+class EditNotePage extends StatefulWidget {
+  final Note note;
+
+  const EditNotePage(this.note, {super.key});
+
+  @override
+  State<StatefulWidget> createState() => _EditNotePageState();
+}
+
+class _EditNotePageState extends State<EditNotePage> {
+  final _formKey = GlobalKey<FormState>();
+
+  final _titleController = TextEditingController();
+  final _textController = TextEditingController();
+
+  @override
+  void initState() {
+    _titleController.text = widget.note.title;
+    _textController.text = widget.note.text;
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    _textController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(elevation: 6, title: Text('Edit Note')),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                TextFormField(
+                  controller: _titleController,
+                  maxLines: 1,
+                  decoration: const InputDecoration(labelText: 'Title', border: OutlineInputBorder()),
+                  validator: (value) => value == null || value.isEmpty ? 'Title required' : null,
+                ),
+                const SizedBox(height: 12),
+                TextFormField(
+                  controller: _textController,
+                  maxLines: 20,
+                  decoration: const InputDecoration(labelText: 'Text', border: OutlineInputBorder(), alignLabelWithHint: true),
+                  validator: (value) => value == null || value.isEmpty ? 'Text required' : null,
+                ),
+                const SizedBox(height: 12),
+                FilledButton.tonalIcon(
+                  onPressed: () async {
+                    if (!_formKey.currentState!.validate()) {
+                      return;
+                    }
+
+                    await NoteService().updateNote(widget.note.id, _titleController.text, _textController.text);
+
+                    widget.note.title = _titleController.text;
+                    widget.note.text = _textController.text;
+
+                    Navigator.of(context).pop(true);
+                  },
+                  icon: Icon(Icons.save),
+                  label: Text('Save'),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
